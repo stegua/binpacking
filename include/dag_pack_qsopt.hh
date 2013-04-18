160684e6c2bb56337111e094fb8ab8cf1d014275
Stefano Gualandi <stefano.gualandi@gmail.com>
2013-04-18 15:34:34 +0200

    dag with qsopt linear solver

---
+++ include/dag_pack_qsopt.hh
@@ -0,0 +1,379 @@
+#ifndef __MY_DAG_PACK
+#define __MY_DAG_PACK
+
+/// From STL library
+#include <vector>
+using std::vector;
+
+#include <stack>
+using std::stack;
+
+#include <queue>
+using std::queue;
+
+/// Numerics limits
+#include <limits>
+using std::numeric_limits;
+
+/// Boost Timer
+#include <boost/progress.hpp>
+using boost::timer;
+
+/// QSOPT
+#include "qsopt.h"
+
+/// My includes
+#include "arc.hh"
+#include "node.hh"
+#include "cost_resources_pack.hh"
+
+#include <stdio.h>
+
+#include <gecode/int.hh>
+using namespace Gecode;
+using namespace Gecode::Int;
+
+#include "cost_multibin.hh"
+using namespace Gecode::Int::CostMultiBinPacking;
+
+/// Constants
+const cost_t EPS = 1e-06;
+
+#include "path.hh"
+
+///--------------------------------------------------------------------------------
+/// Class of graph to compute RCSP with superadditive cost
+class DAG {
+   private:
+      const node_t  n;
+      edge_t  m;
+      const int     k;
+
+      vector<Arc>     A;    /// Arcs  container 
+
+      vector<edge_t>  Pf;   /// Forward  predecessor vector
+      vector<edge_t>  Pb;   /// Backward predecessor vector
+      vector<dist_t>  Df;   /// Forward  distance vector
+      vector<dist_t>  Db;   /// Backward distance vector
+      vector<CostResources> Rf;
+      vector<CostResources> Rb;
+   
+      int      *cind;
+      double   *cval;
+      double   *xbar;
+      QSprob   model;
+
+      /// Initialize distance vector with Infinity
+      /// Maybe it is better to intialize with an upper bound on the optimal path (optimal rcsp path)
+      const dist_t Inf;
+      vector<cost_t> alpha;
+      vector<cost_t> H;
+      cost_t H0;
+      cost_t zb;
+      cost_t f;
+      cost_t UBoff;
+
+   public:
+      vector<Node>  Nc; /// Nodes container
+      resources     U;  /// Upper limits for the *k* resources
+      NodeList      N;  /// List view (Intrusive list)
+     
+      Paths         pool;  /// Set of paths for separation routines
+
+      /// Standard constructor
+      DAG ( node_t _n, edge_t _m, const resources& _U ) 
+         : n(_n), m(_m), k(_U.size()), Pf(n), Pb(n), Df(n), Db(n), Rf(n), Rb(n),
+         Inf(std::numeric_limits<dist_t>::max()), alpha(k,0.0), H(k,0.0),
+         Nc(n), U(_U)
+   {
+      assert( n < std::numeric_limits<node_t>::max()  &&
+            m < std::numeric_limits<edge_t>::max() );
+      /// Reserve memory
+      A.reserve(m);
+      m = 0;
+      /// Initialize the node set
+      for ( node_t i = 0; i < n; ++i ) {
+         Nc[i].setData(i);
+         N.push_back( Nc[i] );
+      }
+      /// Subgradient
+      H0 = 0.0;
+      zb = 0.0;
+      f  = 0.5;
+      /// Cutting planes
+      cind = (int*)malloc(sizeof(int) * (k+1) );
+      cval = (double*)malloc(sizeof(double) * (k+1) );
+      xbar = (double*)malloc(sizeof(double) * (k+1) );
+
+      model = QScreate_prob("lag_continuo", QS_MAX);
+      if (model == NULL) {
+         fprintf(stderr, "Error: could not create the problem\n");
+         exit(EXIT_FAILURE);
+      }
+
+      /// Add the problem variables (u, v_1, ..., v_k)
+      /// Add variable |u|
+      QSnew_col(model, 1.0, -QS_MAXDOUBLE, 100000, (const char*) NULL);
+
+      /// Add variables |v|  
+      for (int i = 0; i < k; ++i) 
+         QSnew_col(model, U[i], -QS_MAXDOUBLE, 0.0, (const char*) NULL);
+   }
+
+      ~DAG () { free(cind); free(cval); free(xbar); QSfree_prob(model); }
+
+      /// Basic getters
+      inline node_t num_nodes(void) const { return N.size(); }
+      inline edge_t num_arcs(void)  const { return A.size(); }
+
+      inline Arc getArc(edge_t id) const { return A[id]; }
+      inline int getKap(void)      const { return k;     }
+
+      inline void setArcCost(edge_t id, cost_t c) { A[id].c = c; }
+
+      /// Basic setter: called only by the constructor
+      edge_t addArc( node_t i, node_t j, cost_t c, const resources& r ) {    
+         edge_t arc_id = static_cast<edge_t>(A.size());
+         /// Added it once for ever
+         A.push_back( Arc() );
+         A[arc_id].setData( arc_id, i, j, c, r );
+         m++;
+         /// Allocated space once for ever and use view to iterate on them
+         Nc[i].addForwArc(A[arc_id]);
+         Nc[j].addBackArc(A[arc_id]);
+         return arc_id;
+      }
+
+      inline void removeArc( FSArcIter a ) {
+         //fprintf(stdout,"removed %d %d\n", a->v, a->w);
+         Nc[a->v].removeForwArc( a );
+         Arc& b = A[a->id];
+         Nc[a->w].removeBackArc( b );
+      }
+
+      inline void removeOppositeArc( FSArcIter a ) {
+         Arc& b = A[a->id];
+         Nc[a->w].removeBackArc( b );
+      }
+
+      inline void removeOppositeArc( BSArcIter a ) {
+         Arc& b = A[a->id];
+         Nc[a->v].removeForwArc( b );
+      }
+
+      edge_t arcsLeft(void) {
+         edge_t left = 0;
+         for ( NodeIter it = N.begin(), it_end = N.end(); it != it_end; ++it ) 
+            left += it->out_degree();
+         return (m-left);
+      }
+
+      ///--------------------------------------------------------------------------------
+      /// Test if the given graph is acyclic
+      bool isAcyclic (void) {
+         return (static_cast<int>(topologicalSort().size()) == n);
+      }
+
+      ///--------------------------------------------------------------------------------
+      /// Shortest Path for a DAG: record only predecessor and length of each node
+      template <class LengthMap>
+         void dag_ssp ( node_t S, const LengthMap& W,
+               vector<edge_t>& P, vector<dist_t>& D ) {    
+            /// Initialize predecessor and distance vectors
+            for ( NodeIter it = N.begin(), it_end = N.end(); it != it_end; ++it ) {
+               P[it->id] = -1;
+               D[it->id] = Inf;
+            }
+            /// Initialize the source distance
+            D[S] = 0;
+
+            /// Get the topological sort 
+            vector<node_t> ts = topologicalSort();
+
+            /// Start the algorithm (*Reaching*, see Denardo and Fox 1979, JSTOR)
+            vector<node_t>::const_iterator it = ts.begin();
+            vector<node_t>::const_iterator it_end = ts.end();
+            while ( it != it_end ) {
+               node_t u = *it++;
+               if ( D[u] < Inf ) { /// If D[u]=Inf then D[u]+Wuv is Inf as well
+                  for ( FSArcIterPair it = Nc[u].getIterFS(); it.first != it.second; ++it.first ) {
+                     node_t v = (it.first)->w;
+                     dist_t Wuv = static_cast<dist_t>(W(*it.first));
+                     if ( D[v] > D[u] + Wuv ) {
+                        D[v] = D[u] + Wuv; 
+                        P[v] = (it.first)->id;
+                     }
+                  }
+               }
+            }
+         }
+
+      ///--------------------------------------------------------------------------------
+      /// Shortest Path for a DAG
+      template <class LengthMap>
+         void
+         dag_ssp_back ( node_t S, const LengthMap& W,
+               vector<edge_t>& P, vector<dist_t>& D ) {
+            /// Initialize predecessor vector
+            for ( NodeIter it = N.begin(), it_end = N.end(); it != it_end; ++it ) {
+               P[it->id] = -1;
+               D[it->id] = Inf;
+            }
+            /// Init the source node
+            D[S] = 0;
+            /// Get the topological sort 
+            vector<node_t> ts = topologicalSortBack();
+            /// Start the algorithm
+            vector<node_t>::const_iterator it = ts.begin();
+            vector<node_t>::const_iterator it_end = ts.end();
+            while ( it != it_end ) {
+               node_t u = *it++;
+               if ( D[u] < Inf ) { /// If D[u]=Inf then D[u]+Wuv is Inf as well
+                  for ( BSArcIterPair it = Nc[u].getIterBS(); it.first != it.second; ++it.first ) {
+                     node_t v = (it.first)->v;   /// It has to consider the arc as reversed
+                     dist_t Wuv = static_cast<dist_t>(W(*it.first));
+                     if ( D[v] > D[u] + Wuv ) {
+                        D[v] = D[u] + Wuv; /// Arc Length
+                        P[v] = (it.first)->id;
+                     }
+                  }
+               }
+            }
+         }
+
+      ///--------------------------------------------------------------------------------
+      /// Shortest Path for a DAG: record only predecessor and length of each node
+      template <class LengthMap>
+         void
+         dag_ssp_all ( node_t S, const LengthMap& W, vector<CostResources>& R,
+               vector<edge_t>& P, vector<dist_t>& D ) {
+            /// Initialize predecessor vector
+            for ( NodeIter it = N.begin(), it_end = N.end(); it != it_end; ++it ) {
+               P[it->id] = -1;
+               D[it->id] = Inf;
+            }
+            /// Init the source node
+            D[S] = 0;
+            /// Get the topological sort 
+            vector<node_t> ts = topologicalSort();    
+            /// Start the algorithm (*Reaching*, see Denardo and Fox 1979, JSTOR)
+            vector<node_t>::const_iterator it = ts.begin();
+            vector<node_t>::const_iterator it_end = ts.end();
+            while ( it != it_end ) {
+               node_t u = *it++;
+               if ( D[u] < Inf ) { /// If D[u]=Inf then D[u]+Wuv is Inf as well
+                  for ( FSArcIterPair it = Nc[u].getIterFS(); it.first != it.second; ++it.first ) {
+                     node_t v = (it.first)->w; /// Arc length (it depends on the LengthView)
+                     dist_t Wuv = static_cast<dist_t>(W(*it.first));
+                     if ( D[v] > D[u] + Wuv ) {
+                        D[v] = D[u] + Wuv; 
+                        P[v] = (it.first)->id;
+                        R[v].add(R[u], *it.first);
+                     }
+                  }
+               }
+            }    
+         }
+
+      ///--------------------------------------------------------------------------------
+      /// Shortest Path for a reversed DAG: record only predecessor and length of each node
+      template <class LengthMap>
+         void
+         dag_ssp_back_all ( node_t S, const LengthMap& W, vector<CostResources>& R,
+               vector<edge_t>& P, vector<dist_t>& D ) {
+            /// Initialize predecessor vector
+            for ( NodeIter it = N.begin(), it_end = N.end(); it != it_end; ++it ) {
+               P[it->id] = -1;
+               D[it->id] = Inf;
+            }
+            /// Init the source node
+            D[S] = 0;
+            /// Get the topological sort 
+            vector<node_t> ts = topologicalSortBack();
+            /// Start the algorithm
+            vector<node_t>::const_iterator it = ts.begin();
+            vector<node_t>::const_iterator it_end = ts.end();
+            while ( it != it_end ) {
+               node_t u = *it++;
+               if ( D[u] < Inf ) { /// If D[u]=Inf then D[u]+Wuv is Inf as well
+                  for ( BSArcIterPair it = Nc[u].getIterBS(); it.first != it.second; ++it.first ) {
+                     node_t v = (it.first)->v;   /// It has to consider the arc as reversed
+                     dist_t Wuv = static_cast<dist_t>(W(*it.first));
+                     if ( D[v] > D[u] + Wuv ) {
+                        D[v] = D[u] + Wuv; /// Arc Length
+                        P[v] = (it.first)->id;
+                        R[v].add(R[u], *it.first);
+                     }
+                  }
+               }
+            }
+         }
+
+      /// Filter the forward star of a vertex
+      template <class LengthMap>
+         bool filterResourceFS(NodeIter vit, const LengthMap& W,
+               const vector<edge_t>& Pf, const vector<edge_t>& Pb,
+               const vector<dist_t>& Df, const vector<dist_t>& Db,
+               const vector<CostResources>& Rf, const vector<CostResources>& Rb,
+               cost_t& UB, int l, node_t Source, node_t Target) 
+         {
+            bool removed = false;
+            node_t v = vit->id;
+            FSArcIterPair it = vit->getIterFS();
+            /// Filter the arcs
+            while ( it.first != it.second ) {
+               FSArcIter a = it.first;
+               ++it.first;  /// First increment the pointer, for safe in-place removals
+               node_t w = a->w; /// Arc length (it depends on the LengthView)
+               dist_t Wuv = static_cast<dist_t>(W(*a));
+               if ( (v != Source && Pf[v] == -1) || (w != Target && Pb[w] == -1) || 
+                     Df[v] + Wuv + Db[w] - EPS > U[l] ) {
+                  removeArc(a);
+                  removed = true;
+               }
+            }
+            return removed;
+         }
+
+      /// Filter the forward star of a vertex with the cost
+      template <class LengthMap>
+         bool filterCostFS(NodeIter vit, const LengthMap& W,
+               const vector<edge_t>& Pf, const vector<edge_t>& Pb,
+               const vector<dist_t>& Df, const vector<dist_t>& Db,
+               const vector<CostResources>& Rf, const vector<CostResources>& Rb,
+               node_t Source, node_t Target, cost_t& UB, cost_t UB_off ) 
+         {
+            bool removed = false;
+            node_t v = vit->id;
+            FSArcIterPair it = vit->getIterFS();
+            /// Filter the arcs
+            while ( it.first != it.second ) {
+               FSArcIter a = it.first;
+               ++it.first;  /// First increment the pointer, for safe in-place removals
+               node_t w = a->w; /// Arc length (it depends on the LengthView)
+               dist_t Wuv = static_cast<dist_t>(W(*a));
+               //fprintf(stdout, "(%d,%d) %.1f %.1f | %.1f %.1f %.1f %.1f\n",v,w,a->c,a->d,Df[v],Wuv,Db[w],UB_off); 
+               if ( (v != Source && Pf[v] == -1) || (w != Target && Pb[w] == -1) || 
+                     computeCost(Df[v]+Wuv+Db[w]+UB_off-EPS) > UB ) {
+                  removeArc(a);
+                  removed = true;
+               }
+            }
+            return removed;
+         }
+
+      /// Methods implemented in the .cc file
+      vector<node_t> topologicalSort(void);
+      vector<node_t> topologicalSortBack(void);
+
+      void clearVertex   ( NodeIter vit );
+      int  subgradient   ( node_t Source, node_t Target, cost_t& LB, cost_t& UB );
+      int  cuttingPlanes ( node_t Source, node_t Target, cost_t& LB, cost_t& UB );
+      int  filter        ( node_t Source, node_t Target, cost_t& LB, cost_t& UB );
+      void printArcs     ( int n, int m ); 
+
+      /// Make it as a propagator   
+      ExecStatus  filterArcs  ( int n, int m, int k, ViewArray<IntView>& x, Space& home);
+}; /// End intrusive graph class
+
+#endif /// __MY_DAG_PACK
---
+++ src/dag_pack_qsopt.cc
@@ -0,0 +1,383 @@
+#include "dag_pack.hh"
+#include "cost_resources_pack.hh"
+#include "path.hh"
+
+#include <cmath>
+
+/// Remove all the arcs incidient in the node given by <vit>
+void
+DAG::clearVertex(NodeIter vit) {
+   for ( FSArcIterPair it = vit->getIterFS(); it.first != it.second; ) {
+      FSArcIter a = it.first;
+      ++it.first;  /// First increment the pointer, for safe in-place removals
+      removeOppositeArc(a);
+   }
+   for ( BSArcIterPair it = vit->getIterBS(); it.first != it.second; ) {
+      BSArcIter a = it.first;
+      ++it.first;  /// First increment the pointer, for safe in-place removals
+      removeOppositeArc(a);
+   }
+   N.erase(vit);
+}
+
+/// Run topological sort and return a vector of topological sorted nodes
+/// [Note: we can do also topological sort via DFS; this can be useful to
+///  explore the verteces in random order... maybe we get a different path
+///  at eah iteration: give it a try!]
+vector<node_t>
+DAG::topologicalSort(void) {
+   vector<node_t> ts;       /// Nodes in topological order
+   ts.reserve(num_nodes());
+   vector<node_t> d(n, 0);  /// Induced vertex incoming degree
+   queue<node_t>  q;        /// Nodes to be explored yet
+   /// Initialize node degrees and scan node without incoming arcs
+   for ( NodeIter it = N.begin(), it_end = N.end(); it != it_end; ++it ) {
+      d[it->id] = it->in_degree();
+      if ( it->in_degree() == 0 )
+         q.push( it->id );
+   }
+   /// Core loop of topological sort
+   while ( !q.empty() ) {
+      node_t v = q.front();
+      q.pop();
+      ts.push_back(v);      
+      for ( FSArcIterPair it = Nc[v].getIterFS(); it.first != it.second; ++it.first ) {
+         node_t w = (it.first)->w;
+         d[w]--;
+         if ( d[w] == 0 )
+            q.push(w);
+      }
+   }
+   return ts;
+}
+
+/// Topological order on the reversed graph
+vector<node_t>
+DAG::topologicalSortBack(void) {
+   vector<node_t> ts;      /// Nodes in topological order
+   ts.reserve(num_nodes());
+   vector<node_t> d(n, 0); /// Induced vertex incoming degree
+   queue<node_t> q;        /// Nodes to be explored yet
+   /// Initialize node degrees and scan node without incoming arcs
+   for ( NodeIter it = N.begin(), it_end = N.end(); it != it_end; ++it ) {
+      d[it->id] = it->out_degree();
+      if ( it->out_degree() == 0 )
+         q.push( it->id );
+   }
+   /// Core loop of topological sort
+   while ( !q.empty() ) {
+      node_t v = q.front();
+      q.pop();
+      ts.push_back(v);
+      for ( BSArcIterPair it = Nc[v].getIterBS(); it.first != it.second; ++it.first ) {
+         node_t w = (it.first)->v;   /// It has to consider the arc as reversed
+         d[w]--;
+         if ( d[w] == 0 )
+            q.push(w);
+      }
+   }
+   return ts;
+}
+
+///--------------------------------------------------------------------------------
+/// Solve the lagrangian subproblem with a subgradient procedure [B&C1989]
+int 
+DAG::subgradient(node_t Source, node_t Target, cost_t& LB, cost_t& UB) {
+   /// Initialize new arc reduced cost
+   ArcGradView W;
+   for ( int v = 0; v < n; ++v ) {
+      Rf[v].setData(0.0,k);
+      Rb[v].setData(0.0,k);
+   }
+   
+   //for ( int l = 0; l < k; ++l ) 
+      //alpha[l] = 0.0;
+   
+   /// Subgradient loop
+   int iter;
+   for ( iter = 0; iter < 50; ++iter ) {
+      //for ( int l = 0; l < k; ++l ) 
+         //fprintf(stdout," %.5f",alpha[l]);
+      //fprintf(stdout," LAGG\n");
+      /// Initialize new arc costs
+      for ( NodeIter nit = N.begin(), nit_end = N.end(); nit != nit_end; ++nit ) {
+         for ( FSArcIterPair it = nit->getIterFS(); it.first != it.second; ++it.first ) {
+            FSArcIter a = it.first;
+            a->d = a->c;
+            for ( int l = 0; l < k; ++l ) 
+               a->d += a->r[l]*alpha[l];
+         }
+      }
+      /// Update offset of the objective function
+      UBoff = 0.0;
+      for ( int l = 0; l < k; ++l ) 
+         UBoff -= alpha[l]*U[l];
+      /// Start double shortest path computation
+      dag_ssp_all(Source, W, Rf, Pf, Df );
+      /// If destination is reachable, update the lower bound
+      if ( Pf[Target] == -1 ) 
+         return 2;
+
+      LB = std::max<cost_t>(LB,Df[Target]+UBoff);
+      if ( fabs(LB-ceil(LB)) > EPS )
+         LB = int(ceil(LB));
+      else 
+         LB = int(round(LB));
+      /// The shortest path is not the optimum path: compute the reversed path
+      dag_ssp_back_all(Target, W, Rb, Pb, Db );
+
+      /// Filter first on the vertex and then on the forward star
+      for ( NodeIter nit = N.begin(), nit_end = N.end(); nit != nit_end; ) {
+         node_t v = nit->id;
+         if ( Df[v] + Db[v] + UBoff - EPS > UB && v != Source && v != Target) {
+            NodeIter vit(nit);
+            ++nit;
+            clearVertex(vit);
+         } else {
+            filterCostFS(nit, W, Pf, Pb, Df, Db, Rf, Rb, Source, Target, UB, UBoff ); 
+            ++nit; 
+         }
+      }
+      /// Update subgradients
+      cost_t Hsqr = 0.0;
+      for ( int l = 0; l < k; ++l ) {
+         H[l] = -U[l] + Rf[Target].r[l];
+         Hsqr += H[l]*H[l];
+      }
+      cost_t T = f*(UB-LB)/Hsqr;
+
+      for ( int l = 0; l < k; ++l )
+         alpha[l] = std::max<cost_t>(0.0, alpha[l]+T*H[l]);
+   }
+
+   if ( iter >= 49 )
+      return 1;
+   return 0;
+}
+
+///--------------------------------------------------------------------------------
+int
+DAG::filter( node_t Source, node_t Target, cost_t& LB, cost_t& UB ) {
+   /// Iterate until an arc is removed
+//   if ( pool.empty() )
+      return cuttingPlanes(Source, Target, LB, UB);
+  // else
+    //  return subgradient(Source, Target, LB, UB);
+}
+
+/// Filter the arcs
+ExecStatus DAG::filterArcs( int n, int m, int k, ViewArray<IntView>& x, Space& home) {
+   for ( int i = 0; i < n; ++i ) {
+      //fprintf(stdout,"\n");
+      if ( !x[i].assigned() ) {
+         int dom_size = 0;
+         int bom_size = 0;
+         int bin = -1;
+         for ( IntVarValues j(x[i]); j(); ++j ) {
+            //fprintf(stdout,"x[%d]=%d\t", i, j.val());
+            if ( Nc[i*m+j.val()].in_degree() > 0 ) {
+               dom_size++;
+               bin = j.val();
+            }
+            if ( Nc[i*m+j.val()].out_degree() > 0 ) {
+               bom_size++;
+               bin = j.val();
+            }
+         }
+         if ( dom_size == 0 || bom_size == 0) { // || (x[i].assigned() && x[i].val() != bin ))
+            return ES_FAILED;
+         }
+
+         if ( dom_size == 1 || bom_size == 1 ) 
+            GECODE_ME_CHECK(x[i].eq(home, bin));
+      }
+   }
+
+   return ES_NOFIX;
+}
+
+///------------------------------------------------------------------------------------------
+/// Compute the lagrangian upper bound and the corresponding dual multipliers
+int 
+DAG::cuttingPlanes ( node_t Source, node_t Target, cost_t& LB, cost_t& UB ) {
+   /// Return status: 0) ok 1) failed 2) filtered
+   int c_status = 0;
+
+   /// Gradient View for the separation algorithm
+   ArcGradView W;
+   for ( int v = 0; v < n; ++v ) {
+      Rf[v].setData(0.0,k);
+      Rb[v].setData(0.0,k);
+   }
+
+   /// Data structure for calling the underlying ANSI/C LP solver
+   int      error  = 0;
+   int      status = 0;
+   double   lb     = 0;
+   int      iter   = 0;
+   double   t0 = 0;
+   double   t1 = 0;
+   double   t_lp = 0;
+   int      nz = 0;
+
+   timer TT;
+
+   if ( pool.empty() ) {
+      /// Constraint on a path for each constraint
+      for ( int l = 0; l < k ; ++l ) {
+         /// Compute the shortest path using the resource consumption
+         dag_ssp(Source, ArcResView(l), Pf, Df);
+         if ( Pf[Target] == -1 ) {
+            c_status = 2;
+            goto QUIT;
+         }
+         /// Make the path
+         Path* path = new Path(*this, Source, Target, Pf);
+         /// First set the variable |u|
+         cind[0] = 0;
+         cval[0] = 1;
+         nz = 1;
+         /// Then set the variables |v|
+         for ( int i = 0; i < k; ++i ) {
+            if ( path->Rc[i] > 0 ) {
+               cind[nz] = i+1;
+               cval[nz] = path->Rc[i];
+               nz++;
+            }
+         }
+         /// Take the rhs
+         double rhs = path->c;
+         error = QSadd_row(model, nz, cind, cval, rhs, 'L', (const char*) NULL);
+         if (error) goto QUIT;
+         pool.push_back(path);
+      }
+   } else {
+      int np = pool.size();
+      for ( int h = 0; h < np; ++h ) {
+         /// Take the rhs
+         double rhs = pool[h].updateCost(*this);
+         error = QSchange_rhscoef(model, h, rhs);
+         if (error) goto QUIT;
+      }
+   } 
+
+   t0 = TT.elapsed();
+
+   //QSset_param (model, QS_PARAM_DUAL_PRICING, QS_PRICE_DDANTZIG);
+   //QSset_param (model, QS_PARAM_DUAL_PRICING, QS_PRICE_DMULTPARTIAL);
+   do {
+      /* Optimize */
+      t1 = TT.elapsed();
+      error = QSopt_dual(model, &status);
+      t_lp += TT.elapsed()-t1;
+      if (error) goto QUIT;
+
+      if (status == QS_LP_UNBOUNDED || status == QS_LP_INFEASIBLE ) {
+         fprintf(stdout, "Unbounded or Infeasible\n");
+         c_status = 1;
+         goto QUIT;
+      }
+
+      /// Take the current LP decision vector
+      error = QSget_x_array(model, xbar);  /// xbar[0] <-> u; xbar[i+1] <-> v_i
+      if (error) goto QUIT;
+
+      double u_bar = xbar[0];
+
+      /// Problema di separazione: Cammino Minimo su DAG
+      /// Set the edge scaled weight as: c_e = c_e - v_1.r_e - ... - v_k.r_e
+      for ( NodeIter nit = N.begin(), nit_end = N.end(); nit != nit_end; ++nit ) {
+         for ( FSArcIterPair it = nit->getIterFS(); it.first != it.second; ++it.first ) {
+            FSArcIter a = it.first;
+            a->d = a->c;
+            for ( int l = 0; l < k; ++l ) 
+               a->d -= xbar[l+1]*a->r[l];
+         }
+      }
+
+      //dag_ssp(Source, W, Pf, Df);
+
+      //if ( Pf[Target] == -1 ) {
+         //c_status = 2;
+         //goto QUIT;
+      //}
+
+      /// Start double shortest path computation
+      dag_ssp_all(Source, W, Rf, Pf, Df );
+      
+      /// If destination is reachable, update the lower bound
+      if ( Pf[Target] == -1 ) {
+         c_status = 2;
+         goto QUIT;
+      }
+      
+      /// Update offset of the objective function
+      UBoff = 0;
+      for ( int l = 0; l < k; ++l ) 
+         UBoff += xbar[l+1]*U[l];
+      LB = std::max<cost_t>(LB,Df[Target]+UBoff);
+      if ( fabs(LB-ceil(LB)) > EPS )
+         LB = int(ceil(LB));
+      else 
+         LB = int(round(LB));
+
+      if ( LB > UB ) {
+         //fprintf(stdout, "Disconnected\n");
+         c_status = 2;
+         goto QUIT;
+      }
+
+      /// The shortest path is not the optimum path: compute the reversed path
+      dag_ssp_back_all(Target, W, Rb, Pb, Db );
+
+      /// Filter first on the vertex and then on the forward star
+      for ( NodeIter nit = N.begin(), nit_end = N.end(); nit != nit_end; ) {
+         node_t v = nit->id;
+         if ( Df[v] + Db[v] + UBoff - EPS > UB && v != Source && v != Target) {
+            NodeIter vit(nit);
+            ++nit;
+            clearVertex(vit);
+         } else {
+            filterCostFS(nit, W, Pf, Pb, Df, Db, Rf, Rb, Source, Target, UB, UBoff ); 
+            ++nit; 
+         }
+      }
+
+      Path* path = new Path(*this, Source, Target, Pf);
+      
+      if ( fabs(path->d - u_bar) < EPS || path->d > u_bar + EPS ) {
+         QSget_objval(model, &lb);
+         goto QUIT;
+      }
+
+      /// First set the variable |u|
+      cind[0] = 0;
+      cval[0] = 1;
+      nz = 1;
+      /// Then set the variables |v|
+      for ( int i = 0; i < k; ++i ) {
+         if ( path->Rc[i] > 0 ) {
+            cind[nz] = i+1;
+            cval[nz] = path->Rc[i];
+            nz++;
+         }
+      }
+      /// Take the rhs
+      double rhs = path->c;
+      error = QSadd_row(model, nz, cind, cval, rhs, 'L', (const char*) NULL);
+      if (error) goto QUIT;
+      pool.push_back(path);
+   } while ( iter++ < 200 );
+
+   if ( iter >= 200 ) 
+      //fprintf(stdout,"WARNING\n");
+      c_status = 1;
+   
+QUIT:
+   if ( c_status == 0 )
+      LB = std::max<cost_t>(lb, LB);
+   for ( int l = 0; l < k; ++l )
+      alpha[l] = fabs(xbar[l+1]);
+//   fprintf(stdout, "TiFilter %.5f %.5f %.5f\n", TT.elapsed(), TT.elapsed()-t0, t_lp); 
+   return c_status;
+}
